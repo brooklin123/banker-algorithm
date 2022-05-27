e9224429e45019141767c3ce8a4c2aef9529620c
brooklin123 <64023999+brooklin123@users.noreply.github.com>
2022-05-27 15:11:31 +0800

    second version

--- banker.cpp
+++ banker.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+//全域變數
 int available[3];
 int allocation[5][3];
 int max_need[5][3];
 int need[5][3];
 vector<int>safety_sequence;
-bool finish[5]; //初始都是false
-//先把Safety會需要的印一印
+bool finish[5];
+
+//全部有的函數名稱
+void print_all_resource();
+bool safety();
+bool banker(int, vector<int>&);
+void recover_from_banker(int, vector<int>&);
+
 void print_all_resource(){
     cout << "Availiable resources: " ;
     for(int i = 0 ; i < 3 ; i ++){
@@ -37,17 +45,14 @@ bool safety(){ // 看能不能找到一組safety sequence
     for(int i = 0 ; i < 5 ; i++){
         finish[i] = false ;
     }
-    cout <<"~~~~~~~~~~~~check safety resource~~~~~~~~~~~~\n";
-    print_all_resource();
-    cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
     int work[3];
     for(int i = 0 ; i < 3 ; i++){
         work[i] = available[i];
     }
-    int fin = 5;
+    int ctr = 5;
     // sequence當某一組成功 那就把他塞進去!!
     while(true){
-        bool canDoIt = 0; //若在此雙層迴圈沒有加一 就代表沒有資源能分配到
+        bool atLeastOne = 0; //若在此雙層迴圈沒有加一 就代表沒有資源能分配到
         for(int i = 0 ; i < 5 ; i++){
             if(!finish[i]){//finish是false
                 bool flag = false; 
@@ -60,19 +65,16 @@ bool safety(){ // 看能不能找到一組safety sequence
                 if(flag == false){ // 確定是safe
                     safety_sequence.push_back(i);
                     finish[i] = true;
-                    fin -= 1;
-                    canDoIt = true;
-                    cout << "work : ";
+                    ctr -= 1;
+                    atLeastOne = true;
                     for(int k = 0 ; k < 3 ; k++){
                         work[k] += allocation[i][k];
-                        cout << work[k] << " ";
                     }
-                    cout <<" process : "<< i << endl;
                 }
             }
         }
-        if(!fin)break; //找到5個true，結束任務 // 記這層回圈 從網上加 
-        if(!canDoIt)return false;
+        if(!ctr)break; //找到5個true，結束任務
+        if(!atLeastOne)return false;
     }
     cout << "Safe! Find safe sequence:";
     for(int i = 0 ; i < safety_sequence.size() ; i++){
@@ -84,60 +86,46 @@ bool safety(){ // 看能不能找到一組safety sequence
 }
 bool banker(int process,vector<int>&request){
     for(int i = 0 ; i < 3 ; i++){
-        if(request[i] > need[process][i] || request[i] > available[i]){
+        if(request[i] > need[process][i]){
+            cout << "No No! you are too greedy\n";
+            return false;
+        }
+        if(request[i] > available[i]){
+            cout <<"Request > available ! process must wait! \n";
             return false;
         }
     }
     cout << endl;
-    // ~~~~~~~~~列印pretend後的值~~~~~~~~~~~~~~~~~~~~~~ 
-    cout << "Pretend to grant the request \n";
-    cout << "Availiable resources: " ;
+    // ~~~~~~~~~pretend~~~~~~~~~~~~~~~~~~~~
     for(int i = 0 ; i < 3 ; i ++){
         available[i] = available[i] - request[i];
-        // cout << available[i] << " ";
     }
-    cout << "\n";
-    cout << "    Resources Allocation | Max Request | Remaining Needs: \n";
-    for(int i = 0 ; i < 5; i++){
-        cout << "Process #" << i << "       ";
-        for(int j = 0 ; j < 3 ; j++){
-            if(i == process){
-                allocation[i][j] = allocation[i][j] + request[j];
-            }
-            cout << allocation[i][j] << " ";     
-        }
-        cout << "  |    ";
-        // max request
-        for(int j = 0 ; j < 3 ; j++){
-            cout << max_need[i][j] << " ";
-        }
-        cout << "   |    ";
-        // pretend remaining needs
-        for(int j = 0 ; j < 3 ; j++){ 
-            if(i == process){
-                need[i][j] = need[i][j] - request[j];
-            } 
-            cout << need[i][j] << " ";
-        }
-        cout << endl;
+    for(int j = 0 ; j < 3 ; j++){
+        allocation[process][j] = allocation[process][j] + request[j];
     }
+    for(int j = 0 ; j < 3 ; j++){ 
+        need[process][j] = need[process][j] - request[j];
+    }
+    cout << "Pretend to grant the request \n";
+    print_all_resource();
     return true;
 }
-void recover_from_banker(int process,vector<int>&request){
-        // ~~~~~~~~~列印pretend後的值~~~~~~~~~~~~~~~~~~~~~~ 
-    cout << "Pretend to grant the request \n";
-    cout << "Availiable resources: " ;
+
+void recover_from_banker(int process,vector<int>&request){ 
+    cout << "enter the recovery section\n";
     for(int i = 0 ; i < 3 ; i ++){
         available[i] = available[i] + request[i];
     }
     for(int j = 0 ; j < 3 ; j++){
         allocation[process][j] = allocation[process][j] - request[j];
     }   
-        // pretend remaining needs
+    // pretend remaining needs
     for(int j = 0 ; j < 3 ; j++){ 
         need[process][j] = need[process][j] + request[j];
     }
+    cout <<"recovery has completed\n";
 }
+
 int main(){
     int a,b,c;
     cout << "=== Banker's algorithm starts! \n";
@@ -167,10 +155,7 @@ int main(){
     if(!is_Safe){
         return 0;//Exit!
     }
-    //第一頁的""RESOURCE-REQUEST algrithm stats!"
-    // ....
     cout << "Safe! Grant the request\n";
-    //起董循環~
     while(true){
         char isContine;
         int process;
@@ -183,6 +168,7 @@ int main(){
         }
         cout << "Request! Enter process no: ";
         cin >> process;
+        cout <<endl;
         for(int i = 0 ; i < 3 ; i++){
             cout << "Enter request resource #" << i << " = ";
             cin >> change_allocate[i];
@@ -190,18 +176,15 @@ int main(){
         }
         if(banker(process,change_allocate)){ // banker algo過了 再做safety
             cout << "== Safety algorithm starts!\n\n";
-            bool isSafe = safety(); //??? 第二次發 process:1 quest : 102 是false???
+            bool isSafe = safety();
             if(!isSafe){
-                cout << "Unsafe. Do not grant! Restore the allocation state! \n";//unsafe 要recover
+                cout << "Unsafe. Do not grant! Restore the allocation state! \n";
                 recover_from_banker(process, change_allocate);
             }
             else{
                 cout << "corret! you pass the banker's algo \n";
             }
         }
-        else{
-            cout << "No No! you are too greedy\n";
-        }
-    } // 當不繼續 那就可以跳出此while~
+    }
     return 0;
 }
